bbe80eabd37d1db9478df580513d302163f5269e
Brook <birukbr99@yahoo.com>
2015-01-05 00:30:19 -0600

    restfulclient added

---
+++ src/main/java/com/onlinebazzar/paymentgt/restfulclient/RestClient.java
@@ -0,0 +1,65 @@
+package com.onlinebazzar.paymentgt.restfulclient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class RestClient {
+
+	
+	
+	public static int validateCC(String cardno, double totalPrice) {
+        
+		  	int result=0;
+	
+	  try{  
+	        URL url = new URL(makeURL(cardno, totalPrice));
+//	      URLConnection uc = url.openConnection();
+	        HttpURLConnection uc = (HttpURLConnection) url.openConnection();
+
+	        uc.setRequestMethod("GET");
+	        uc.setDoOutput(true);
+	        uc.setRequestProperty( "Content-type", "text/plain" );
+	        uc.setRequestProperty( "Accept", "text/plain" );
+	   
+	        int rspCode = uc.getResponseCode();
+
+	        if (rspCode == 200) {
+	            InputStream is = (InputStream) uc.getInputStream();
+	            InputStreamReader isr = new InputStreamReader(is);
+	            BufferedReader br = new BufferedReader(isr);
+
+	            String nextLine = br.readLine();
+	            while (nextLine != null) {
+	                result=Integer.parseInt(nextLine);
+	                nextLine = br.readLine();
+	            }
+
+	        }
+	        }
+
+	        catch(IOException e) { 
+	            e.printStackTrace();
+	        }
+	return result;
+
+	    }
+	private static String makeURL(String cardNumber, double amount){
+        String url = "http://10.10.34.185:8080/PayementService/webresources/com.mycompany."
+        		+ "payementservice.accountdetails"
+        		+ "/cValidation?cardnumber="+cardNumber+"&totalprice=+"+amount;
+
+		return url;
+		
+	}
+	
+	public static void main(String[] args) {
+		int result = RestClient.validateCC("3333444455556666", 200);
+		System.out.println(result);
+	}
+
+}
