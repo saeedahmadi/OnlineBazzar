a5acd8710eab5cc39f27cf6e40d4778968162c72
Saeed Ahmadi <saeed_csd@yahoo.com>
2015-01-08 22:24:47 -0600

    Tests added for test data

--- src/test/java/com/onlinebazzar/services/test/CategoryServiceImplTest.java
+++ src/test/java/com/onlinebazzar/services/test/CategoryServiceImplTest.java
@@ -11,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.onlinebazzar.model.Category;
+import com.onlinebazzar.model.Person;
+import com.onlinebazzar.model.Product;
+import com.onlinebazzar.model.Vendor;
 import com.onlinebazzar.services.CategoryService;
 
 
@@ -23,6 +26,43 @@ public class CategoryServiceImplTest  extends ServiceImplTestBase{
 	@Test
 	@Transactional
 	public void testSaveCategory() {
+		String categoryNames [] = new String[5];
+		categoryNames[0]="Shoes";
+		categoryNames[1]="Watches";
+		categoryNames[2]="Ties";
+		categoryNames[3]="Pants";
+		categoryNames[4]="Shirts";
+		
+		String vendorNames [] = new String[5];
+		vendorNames[0]="Nike";
+		vendorNames[1]="Rolex";
+		vendorNames[2]="Macy";
+		vendorNames[3]="Levis";
+		vendorNames[4]="Calvin Clark";
+		
+		personService.save(createOneNewAdmin());
+		customerService.save(createOneNewCustomer());
+		for(int i=0;i<categoryNames.length;i++){
+			Category c = createOneNewCategory(categoryNames[i]);
+			c = categoryService.save(c);
+			
+			Vendor v = createOneNewVendor(vendorNames[i]);
+			
+			v = vendorService.save(v);
+			Person person = createOneNewVendorAdmin(v);
+			personService.save(person);
+			
+			for(int j=1; j<=5;j++){
+				Product p = createOneNewProduct(c,v);
+				productService.save(p);
+			}
+		}
+		
+	}
+	
+	/*@Test
+	@Transactional
+	public void testSaveCategory() {
 		Category c = createOneNewCategory();
 		c = categoryService.save(c);
 		Category c2 = categoryService.findOne(c.getId());
@@ -31,9 +71,9 @@ public class CategoryServiceImplTest  extends ServiceImplTestBase{
 		//customerService.deleteCustomer(c2.getId());
 		//Customer c3 = customerService.getCustomer(c.getId());
 		//assertNull(c3);
-	}
+	}*/
 
-	@Test
+	/*@Test
 	@Transactional
 	public void testListCustomer() {
 		List<Category> categoryList = categoryService.findAll();
@@ -41,6 +81,6 @@ public class CategoryServiceImplTest  extends ServiceImplTestBase{
 		c = categoryService.save(c);
 		List<Category> cList = categoryService.findAll();
 		assertEquals(cList.size(), categoryList.size()+1);
-	}
+	}*/
 
 }
